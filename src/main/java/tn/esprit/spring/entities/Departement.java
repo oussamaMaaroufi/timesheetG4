5433b7e8821d002258cc64934ddf12acf7ec51f4
oussamaMaaroufi <oussama.maaroufi@esprit.tn>
2021-11-15 02:11:22 +0100

    01:28 15/11/2021

--- src/main/java/tn/esprit/spring/entities/Departement.java
+++ src/main/java/tn/esprit/spring/entities/Departement.java
@@ -17,7 +17,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 @Entity
 public class Departement implements Serializable {
 
-	private static final long serialVersionUID = -357738161698377833L;
+	//private static final long serialVersionUID = -357738161698377833L;
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
